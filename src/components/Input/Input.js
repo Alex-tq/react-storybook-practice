1955e372588e217dbaebb462e881ae65e1b09655
Alexander Tamarez <alextq3036@gmail.com>
2022-02-16 23:05:50 -0500

    Fixed css import

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Input.css";
 
 function Input(props) {
-  const { size, ...rest } = props;
-  return <input type="text" className={size} {...rest} />;
+  const { size = "medium", ...rest } = props;
+  return <input type="text" className={`${size}`} {...rest} />;
 }
 
 export default Input;
